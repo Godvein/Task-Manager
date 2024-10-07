bfcae9493ee6fdd89d0bf77afcf3fcac63b41c28
Ankit Dware <sabindware@gmail.com>
2024-10-07 20:05:54 +0545

    final update
    linked task model with user model using foreign key so that user can perform crud on their task only

--- tasks/models.py
+++ tasks/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.contrib.auth.models import User
 # Create your models here.
 class Tasks(models.Model):
+    user = models.ForeignKey(User, on_delete = models.CASCADE, null = True)
     id = models.AutoField(primary_key=True)
     name = models.TextField(max_length=50)
     description = models.TextField(max_length=500)
--- tasks/views.py
+++ tasks/views.py
@@ -1,12 +1,13 @@
-from django.shortcuts import render,redirect
+from django.shortcuts import render,redirect, get_object_or_404
 from .models import Tasks
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth import logout
+from django.contrib.auth.models import User
 # Create your views here.
 @login_required(login_url="/login/")
 def home(request):
-   taskmodel = Tasks.objects.count()
+   taskmodel = Tasks.objects.filter(user = request.user).count()
    numdict = {
       "taskmodel" : taskmodel
    }
@@ -17,13 +18,13 @@ def addTasks(request):
    if request.method == "POST":
       name = request.POST.get("name")
       description = request.POST.get("description")
-      query = Tasks(name = name, description = description)
+      query = Tasks(user = request.user, name = name, description = description)
       query.save()
    return render(request, "addtasks.html")
 
 @login_required(login_url="/login/")
 def viewTasks(request):
-   taskmodel = Tasks.objects.all()
+   taskmodel = Tasks.objects.filter(user=request.user)
    alltasks = {
       "tasks" : taskmodel
    }
@@ -31,20 +32,20 @@ def viewTasks(request):
 
 @login_required(login_url="/login/")
 def completeTasks(request, id):
-   taskmodel = Tasks.objects.get(id = id)
+   taskmodel = get_object_or_404(Tasks, id=id, user=request.user)
    taskmodel.delete()
    messages.success(request, "Congratulations on completing the task: " + taskmodel.name)
    return redirect("/tasks/")
 
 @login_required(login_url="/login/")
 def deleteTasks(request, id):
-   taskmodel = Tasks.objects.get(id = id)
+   taskmodel = get_object_or_404(Tasks, id=id, user=request.user)
    taskmodel.delete()
    return redirect("/tasks/")
 
 @login_required(login_url="/login/")
 def editTask(request, id):
-   taskmodel = Tasks.objects.get(id = id)
+   taskmodel = get_object_or_404(Tasks, id=id, user=request.user)
    task = {
       "taskmodel" : taskmodel
    }
